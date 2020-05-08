6c86d239a258bcfcb50f0428daffec59e71a7712
Ifedayo Fasakin <fasakin.ifedayo@gmail.com>
2020-05-08 11:48:47 +0100

    setting up mongoose and created model class

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose")
+const keys = require('./config/keys')
+require("./services/passport");
+require('./models/user')
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true})
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Hello world" });
-});
+require("./routes/authRoutes")(app);
 
-app.listen(5000, () => {
-  console.log("Web server is listening on port 5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
