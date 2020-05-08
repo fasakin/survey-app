d2f8941adae0e10489a9fa70b6fd7d2349aed95b
Ifedayo Fasakin <fasakin.ifedayo@gmail.com>
2020-05-08 17:52:49 +0100

    cookie-session setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
-const express = require("express");
-const mongoose = require("mongoose")
+const express = require('express');
+const mongoose = require('mongoose')
+const passport = require('passport')
+const cookieSection = require('cookie-session')
 const keys = require('./config/keys')
-require("./services/passport");
 require('./models/user')
+require("./services/passport");
 
 
 mongoose.connect(keys.mongoURI)
 const app = express();
+
+app.use(cookieSection({
+    maxAge: 30 *24*60*60*1000,
+    keys: [keys.cookieKeys]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 require("./routes/authRoutes")(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require('mongoose')
 const keys = require("../config/keys");
+
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+  .then(user => done(null, user))
+})
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +23,18 @@ passport.use(
         callbackURL: "/auth/google/callback",
       },
       (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken:', accessToken);
-        console.log('refreshToken:', refreshToken);
-        console.log('profile:', profile);
+        User.findOne({googleId: profile.id})
+        .then( existingUser =>{
+          if( existingUser ) {
+            //already a recond in the database
+            done(null, existingUser)
+          } else {
+            //create new user
+            new User({googleId: profile.id }).save()
+            .then(user => done(null, user))
+          }
+        } )
+        
       }
     )
   );
